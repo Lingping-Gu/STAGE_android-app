522727697787ac48a1513775bfcf1820653129b2
Tenzin Kunsang <tenzkunsang648@gmail.com>
2023-11-15 22:59:05 -0500

    fragmented explore

--- app/src/main/java/edu/northeastern/stage/ui/explore/ExploreFragment.java
+++ app/src/main/java/edu/northeastern/stage/ui/explore/ExploreFragment.java
@@ -3,40 +3,131 @@ package edu.northeastern.stage.ui.explore;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.SeekBar;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import edu.northeastern.stage.Circle;
+import edu.northeastern.stage.CircleView;
+import edu.northeastern.stage.Explore;
 import edu.northeastern.stage.R;
-import edu.northeastern.stage.databinding.FragmentExploreBinding;
-import edu.northeastern.stage.ui.musicReview.MusicReviewFragment;
+import edu.northeastern.stage.Review;
 
 public class ExploreFragment extends Fragment {
 
-    private FragmentExploreBinding binding;
+    private ArrayAdapter<String> adapter;
+    private AutoCompleteTextView actv;
+    private Button reviewButton;
+    private CircleView circleView;
+    private SeekBar geoSlider;
     private ExploreViewModel viewModel;
+    private static final String TAG = Explore.class.getSimpleName();
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_explore, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_explore, container, false);
 
-        Button buttonToMusicReview = view.findViewById(R.id.test_button);
+        reviewButton = fragmentView.findViewById(R.id.reviewButton);
+        circleView = fragmentView.findViewById(R.id.circleView);
+        geoSlider = fragmentView.findViewById(R.id.slider);
+        actv = fragmentView.findViewById(R.id.autoCompleteTextView);
+        actv.setThreshold(1);
 
-        buttonToMusicReview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Use the NavController to navigate to the MusicReviewFragment
-                NavController navController = NavHostFragment.findNavController(ExploreFragment.this);
-                navController.navigate(R.id.action_navigation_explore_to_navigation_music_review);
-            }
+        viewModel = new ViewModelProvider(this).get(ExploreViewModel.class);
+        observeViewModel();
+
+        actv.addTextChangedListener(textWatcher);
+
+        actv.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedSong = (String) parent.getItemAtPosition(position);
+            viewModel.songSelected(selectedSong);
+            reviewButton.setEnabled(true);
         });
-        return view;
+
+        reviewButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), Review.class);
+            startActivity(intent);
+        });
+
+        viewModel.setCircles(createCircles());
+
+        return fragmentView;
     }
+
+    TextWatcher textWatcher = new TextWatcher() {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            Log.d(TAG, "in beforeTextChanged");
+            reviewButton.setEnabled(false);
+            if(s.length() == 0){
+//                resultText.setText("");
+            }
+            // This function is called before text is edited
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            Log.d(TAG, "in onTextChanged");
+            // This function is called when text is edited
+//            toastMsg("Text is edited, and onTextChangedListener is called.");
+            if(s.length() == 0){
+//                resultText.setText("");
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            Log.d(TAG, "in afterTextChanged");
+
+            viewModel.searchTextChanged(s.toString());
+
+        }
+    };
+
+    private void observeViewModel() {
+        viewModel.getRecommendations().observe(getViewLifecycleOwner(), recommendations -> {
+            adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, recommendations);
+            actv.setAdapter(adapter);
+        });
+    }
+
+    public List<Circle> createCircles(){
+        List<Circle> circles = new ArrayList<>();
+
+        Random rand = new Random();
+
+        circles.add(new Circle(circleView.getHeight()/2, circleView.getWidth()/2, 55));
+
+        for(int i=1; i<50; i++) {
+
+            float x = rand.nextFloat()*100;
+            float y = rand.nextFloat()*100;
+            float radius = rand.nextFloat()*100 + 5;
+
+            circles.add(new Circle(x, y, radius));
+        }
+
+        // Set the circles to the existing CircleView
+        if (circleView != null) {
+            circleView.setCircles(circles);
+            circleView.invalidate(); // Request a redraw
+        }
+        return circles;
+    }
+
 }
--- app/src/main/java/edu/northeastern/stage/ui/explore/ExploreViewModel.java
+++ app/src/main/java/edu/northeastern/stage/ui/explore/ExploreViewModel.java
@@ -1,7 +1,119 @@
 package edu.northeastern.stage.ui.explore;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class ExploreViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+import edu.northeastern.stage.Circle;
+import edu.northeastern.stage.R;
+
+public class ExploreViewModel extends AndroidViewModel {
+
+    private MutableLiveData<List<String>> recommendations = new MutableLiveData<>();
+    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
+    private MutableLiveData<String> selectedSong = new MutableLiveData<>();
+
+    public ExploreViewModel(Application application) {
+        super(application);
+    }
+
+    public LiveData<List<String>> getRecommendations() {
+        return recommendations;
+    }
+
+    public LiveData<Boolean> getIsLoading() {
+        return isLoading;
+    }
+
+    public void searchTextChanged(String text) {
+        if (text.isEmpty()) {
+            recommendations.setValue(new ArrayList<>());
+        } else {
+            makeDeezerReq(text);
+        }
+    }
+
+    public void songSelected(String song) {
+        selectedSong.setValue(song);
+        // Any other logic related to song selection
+    }
+
+    public LiveData<String> getSelectedSong() {
+        return selectedSong;
+    }
+
+    public void makeDeezerReq(String inputArtistName) {
+        String deezerApiKey = getApplication().getString(R.string.DEEZER_API);
+        isLoading.setValue(true);
+
+        new Thread(() -> {
+            try {
+                URL url = new URL(getApplication().getString(R.string.DEEZER_BASE_URL) + inputArtistName);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("X-RapidAPI-Key", deezerApiKey);
+                urlConnection.setRequestProperty("X-RapidAPI-Host", "deezerdevs-deezer.p.rapidapi.com");
+                urlConnection.setDoInput(true);
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                String jsonString = convertStreamToString(inputStream);
+                parseJsonAndUpdate(jsonString);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                recommendations.postValue(new ArrayList<>());
+            } finally {
+                isLoading.postValue(false);
+            }
+        }).start();
+    }
+
+
+    private void parseJsonAndUpdate(String jsonString) {
+        try {
+            JSONObject response = new JSONObject(jsonString);
+            JSONArray data = response.getJSONArray("data");
+            List<String> tempRecs = new ArrayList<>();
+            for (int i = 0; i < data.length(); i++) {
+                JSONObject currentResult = data.getJSONObject(i);
+                JSONObject artist = currentResult.getJSONObject("artist");
+                String trackName = currentResult.getString("title");
+                String artistName = artist.getString("name");
+                tempRecs.add(trackName + " by " + artistName);
+            }
+            recommendations.postValue(tempRecs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String convertStreamToString(InputStream is) {
+        Scanner s = new Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next().replace(",", ",\n") : "";
+    }
+
+
+    public void setCircles(List<Circle> circles) {
+        // Process the circles as needed in the ViewModel
+        // This method can be called from the ExploreFragment
+    }
 }
