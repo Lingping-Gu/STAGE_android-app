a7a7a73430bc9e22a673c6d7497cf307dd9ee7b0
Lingping-Gu <gu.ling@northeastern.edu>
2023-11-15 22:32:36 -0800

    post function update

--- app/src/main/java/edu/northeastern/stage/ui/profile/ProfileFragment.java
+++ app/src/main/java/edu/northeastern/stage/ui/profile/ProfileFragment.java
@@ -11,10 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import edu.northeastern.stage.EditProfile;
+import edu.northeastern.stage.R;
+import edu.northeastern.stage.model.Post;
 import edu.northeastern.stage.databinding.FragmentProfileBinding;
+
 import android.content.Intent;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -85,6 +92,51 @@ public class ProfileFragment extends Fragment {
             // Update the RecyclerView with new tags
             tagsAdapter.setTags(tags);
             tagsAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public static class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostViewHolder> {
+
+        private List<Post> postList;
+
+        public PostsAdapter(List<Post> postList) {
+            this.postList = postList;
+        }
+
+        @Override
+        public PostViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_layout, parent, false);
+            return new PostViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(PostViewHolder holder, int position) {
+            Post post = postList.get(position);
+            // Set the data to your views here
+            holder.tvMusicLink.setText(post.getMusicLink());
+            holder.tvPostContent.setText(post.getPostContent());
+            // Set other views like image views etc.
+        }
+
+        @Override
+        public int getItemCount() {
+            return postList.size();
+        }
+
+        public static class PostViewHolder extends RecyclerView.ViewHolder {
+
+            TextView tvMusicLink, tvPostContent;
+            ImageView ivUserAvatar, ivLike, visibleState;
+
+            public PostViewHolder(View itemView) {
+                super(itemView);
+                tvMusicLink = itemView.findViewById(R.id.tvMusicLink);
+                tvPostContent = itemView.findViewById(R.id.tvPostContent);
+                ivUserAvatar = itemView.findViewById(R.id.ivUserAvatar);
+                ivLike = itemView.findViewById(R.id.ivLike);
+                visibleState = itemView.findViewById(R.id.visibleState);
+                // Initialize other views
+            }
         }
     }
 }
