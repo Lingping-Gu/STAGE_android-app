30704caacc25ce52fee79b7bf4dbd8fad65b8479
BINGCHUN CHANG <122491347+CharlieCBC@users.noreply.github.com>
2023-11-28 23:06:55 -0500

    Adapt deezer search in new post page

--- app/src/main/java/edu/northeastern/stage/ui/newPost/NewPostFragment.java
+++ app/src/main/java/edu/northeastern/stage/ui/newPost/NewPostFragment.java
@@ -1,15 +1,19 @@
 package edu.northeastern.stage.ui.newPost;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import edu.northeastern.stage.R;
 import edu.northeastern.stage.databinding.FragmentNewPostBinding;
 
 public class NewPostFragment extends Fragment {
@@ -22,42 +26,45 @@ public class NewPostFragment extends Fragment {
 
         viewModel = new ViewModelProvider(this).get(NewPostViewModel.class);
 
+        // Initialize resources
+        String deezerBaseUrl = getString(R.string.DEEZER_BASE_URL);
+        String deezerApiKey = getString(R.string.DEEZER_API);
+
         // Set up the interactions for the new post elements
-        binding.btnSubmitPost.setOnClickListener(v -> {
-            // Use the ViewModel to handle post submission
+        binding.btnSubmitPost.setOnClickListener(v -> viewModel.submitPost(binding.etPostContent.getText().toString()));
 
-//            viewModel.submitPost(/* post content here */);
-        });
-
-        // Set up the SearchView listener using the ViewModel
-        binding.svSongSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                // Use the ViewModel to perform the search
-                viewModel.performSearch(query).observe(getViewLifecycleOwner(), searchResults -> {
-                    // Update UI with the search results
-                });
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                // React to text change needed
-                return false;
-            }
-        });
+        // Setup AutoCompleteTextView for song search
+        setupSearch(deezerBaseUrl, deezerApiKey);
 
         // Observe the post submission status from the ViewModel
         viewModel.getPostSubmissionStatus().observe(getViewLifecycleOwner(), isSuccess -> {
-            // Update UI based on submission status
-            if (isSuccess) {
-                // Show success message or transition to another screen
-            } else {
-                // Show error message
-            }
+            // Handle post submission status
         });
 
         return root;
+    }
+
+    private void setupSearch(String deezerBaseUrl, String deezerApiKey) {
+        ArrayAdapter<String> searchAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line);
+        binding.actvSongSearch.setAdapter(searchAdapter);
+
+        binding.actvSongSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                viewModel.performSearch(s.toString(), deezerBaseUrl, deezerApiKey)
+                        .observe(getViewLifecycleOwner(), searchResults -> {
+                            searchAdapter.clear();
+                            searchAdapter.addAll(searchResults);
+                            searchAdapter.notifyDataSetChanged();
+                        });
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
     }
 
     @Override
