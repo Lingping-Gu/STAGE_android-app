35010c226bc28690b40f43c6ea8ba2dc378b5685
Lingping-Gu <gu.ling@northeastern.edu>
2023-11-29 17:25:58 -0800

    update for merge to main

--- app/src/main/java/edu/northeastern/stage/ui/adapters/PostAdapter.java
+++ app/src/main/java/edu/northeastern/stage/ui/adapters/PostAdapter.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
