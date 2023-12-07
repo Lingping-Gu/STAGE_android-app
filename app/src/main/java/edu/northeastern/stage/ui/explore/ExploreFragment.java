cfd10ae05e255df81bd308abbbbbcb46488c74dd
BINGCHUN CHANG <122491347+CharlieCBC@users.noreply.github.com>
2023-12-06 19:13:52 -0500

    Update ExploreFragment.java

--- app/src/main/java/edu/northeastern/stage/ui/explore/ExploreFragment.java
+++ app/src/main/java/edu/northeastern/stage/ui/explore/ExploreFragment.java
@@ -167,9 +167,9 @@ public class ExploreFragment extends Fragment {
         actv.setOnItemClickListener((parent, view, position, id) -> {
             selectedTrack = searchAdapter.getItem(position);
             if (selectedTrack != null) {
+                String artists = "";
                 try {
                     Log.d("ExploreFragment", "onItemClick - Selected track: " + selectedTrack);
-                    String artists = "";
                     JsonArray artistsArray = selectedTrack.getAsJsonArray("artists");
                     if (artistsArray != null && artistsArray.size() > 0) {
                         for (JsonElement artist : artistsArray) {
