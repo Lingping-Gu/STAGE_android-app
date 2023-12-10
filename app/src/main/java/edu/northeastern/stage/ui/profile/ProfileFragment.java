cd9c3117454523cef9835cc59cfe6a9f17fbb720
sam <lee.samuel5@northeastern.edu>
2023-12-09 21:12:08 -0500

    Small changes in terms of variable names

--- app/src/main/java/edu/northeastern/stage/ui/profile/ProfileFragment.java
+++ app/src/main/java/edu/northeastern/stage/ui/profile/ProfileFragment.java
@@ -14,7 +14,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import edu.northeastern.stage.MainActivity;
 import edu.northeastern.stage.ui.adapters.PostAdapter;
 import edu.northeastern.stage.R;
 import edu.northeastern.stage.model.Post;
