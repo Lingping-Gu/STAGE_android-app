0c24f2929d2123d42fe12671ecc5e7882619976d
sam <lee.samuel5@northeastern.edu>
2023-12-09 23:55:06 -0500

    fix search adapter part 3

--- app/src/main/java/edu/northeastern/stage/ui/explore/ExploreFragment.java
+++ app/src/main/java/edu/northeastern/stage/ui/explore/ExploreFragment.java
@@ -43,8 +43,6 @@ public class ExploreFragment extends Fragment {
     private TextView progressTextView;
     private SharedDataViewModel sharedDataViewModel;
     TrackSearchAdapter searchAdapter;
-    private static final int SEARCH_DELAY = 500;
-    private long lastSearchTime = 0;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -70,16 +68,12 @@ public class ExploreFragment extends Fragment {
             }
         });
 
-        searchAdapter = new TrackSearchAdapter(getContext(), actv);
         //setup search
         setupSearch();
 
         buttonToMusicReview.setOnClickListener(v -> {
             if(!actv.getText().toString().isEmpty()) {
                 actv.setText("");
-//                NavController navController = NavHostFragment.findNavController(ExploreFragment.this);
-//                navController.navigate(R.id.action_navigation_explore_to_navigation_music_review);
-                // Use the manual navigation.
                 ((MainActivity)requireActivity()).navigateToFragment("MUSIC_REVIEW_FRAGMENT", true, null);
             }
         });
@@ -120,8 +114,6 @@ public class ExploreFragment extends Fragment {
     }
 
     private void setupSearch() {
-        actv.setAdapter(searchAdapter);
-
         actv.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -132,33 +124,24 @@ public class ExploreFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.length() == 0){
-                }
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-
                 try {
-                    long currentTime = System.currentTimeMillis();
-                    // add delay of 500 ms between current time and last search time for efficiency
-                    // search length should be more than 0
-                    if(currentTime - lastSearchTime > SEARCH_DELAY && s.length() != 0) {
-                        lastSearchTime = currentTime;
-                        actv.showDropDown();
-
-                        Log.d("ExploreFragment", "afterTextChanged - Performing search for: " + s.toString());
+                    Log.d("ABC123",String.valueOf(s.length()));
+                    if(s.length() != 0) {
                         viewModel.performSearch(s.toString())
                                 .observe(getViewLifecycleOwner(), searchResults -> {
-                                    Log.d("ExploreFragment", "afterTextChanged - SEARCH RESULTS ->  " + searchResults);
-
-                                    List<JsonObject> results = new ArrayList<>();
+                                    ArrayList<JsonObject> results = new ArrayList<>();
 
                                     for (int i = 0; i < searchResults.size(); i++) {
                                         Log.d("ExploreFragment", "afterTextChanged - LOOP " + searchResults.get(i).get("name").getAsString() + " BY " + searchResults.get(i).getAsJsonArray("artists").get(0).getAsJsonObject().get("name").getAsString());
                                         results.add(searchResults.get(i).getAsJsonObject());
                                     }
-                                    searchAdapter.setSelectedResult(results);
+                                    searchAdapter = new TrackSearchAdapter(getContext(),results);
+                                    actv.setAdapter(searchAdapter);
+                                    searchAdapter.notifyDataSetChanged();
                                 });
                     }
                 } catch (Exception e) {
